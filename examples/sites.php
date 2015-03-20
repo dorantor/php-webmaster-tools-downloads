80276ec41633fefe6f73cf92b8bb1bf5c705b29b
Anton Kolenkov <dorantor@gmail.com>
2015-03-21 01:13:24 +0400

    Updated sites example.

--- examples/sites.php
+++ examples/sites.php
@@ -1,15 +1,17 @@
 <?php
 
 include '../src/Gwt/Client.php';
+// load config values
+include 'config.sample.php';
+
 
 try {
-
     $sites = Gwt_Client::create($email, $password)
         ->getSites()
     ;
 
-    foreach($sites as $site) {
-        echo $site, "\n";
+    foreach ($sites as $site => $options) {
+        echo $site, ' : ', ($options['verified'] ? 'verified' : 'not verified') ,"\n";
     }
 } catch (Exception $e) {
     die($e->getMessage());

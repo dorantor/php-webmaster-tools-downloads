f599f7cb68cb4bded6c2c592e0d6dbec292a302a
Anton Kolenkov <dorantor@gmail.com>
2015-03-20 22:19:11 +0400

    Added files required for updated examples.

---
+++ examples/config.sample.php
@@ -0,0 +1,8 @@
+<?php
+
+$email    = 'example@gmail.com';
+$password = '********'; // use app password if you have two step verification
+$website  = 'http://example.com/'; // use the same value you get from ->getSites()
+$language = 'en';
+
+include 'config.php'; // feel free to create this file to override default settings

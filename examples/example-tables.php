bb27da042c81416bb4ae3bbd9c0b8c891cc63865
Anton Kolenkov <dorantor@gmail.com>
2015-03-21 06:51:37 +0400

    Updated tables example.

--- examples/example-tables.php
+++ examples/example-tables.php
@@ -1,23 +1,33 @@
 <?php
 
 include '../src/Gwt/Client.php';
+include '../src/Gwt/Processor/ProcessorInterface.php';
+include '../src/Gwt/Processor/ProcessorAbstract.php';
+include '../src/Gwt/Processor/CsvWriter.php';
+// load config values
+include 'config.sample.php';
 
 try {
-    $email = 'username@gmail.com';
-    $passwd = '******';
-
-    # If hardcoded, don't forget trailing slash!
-    $website = 'http://www.domain.com/';
-
     # Valid values are 'TOP_PAGES', 'TOP_QUERIES', 'CRAWL_ERRORS',
     # 'CONTENT_ERRORS', 'CONTENT_KEYWORDS', 'INTERNAL_LINKS',
     # 'EXTERNAL_LINKS' and 'SOCIAL_ACTIVITY'.
     $tables = array('TOP_QUERIES');
 
-    Gwt_Client::create($email, $passwd)
-        ->setTables($tables)
-        ->downloadCSV($website)
+    $client = Gwt_Client::create($email, $passwd)
+        ->setDaterange(
+            new DateTime('-10 day', new DateTimeZone('UTC')),
+            new DateTime('-9 day',  new DateTimeZone('UTC'))
+        )
+        ->addProcessor(
+            Gwt_Processor_CsvWriter::factory(array(
+                'savePath'          => '.',
+                'dateFormat'        => 'Ymd',
+                'filenameTemplate'  => '{website}' . DIRECTORY_SEPARATOR . '{tableName}-{dateStart}-{dateEnd}.csv',
+            ))
+        )
     ;
+
+    $filenames = $client->getTableData($tableName);
 
 } catch (Exception $e) {
     die($e->getMessage());
